925bd49d8cf945d7d73c820cca0f6c7d0060b869
aquaHsu <bellems811@gmail.com>
2022-07-19 10:36:27 +0800

    update sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div class="h-full flex">
+  <SideNav />
+  <Home />
+</div>
+
 </template>
+<script>
+
+import SideNav from './components/SideNav.vue';
+import Home from './views/Home.vue'
+export default {
+  components:{
+    SideNav,
+    Home
+  }
+}
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  height: 100%;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+html, body {
+  height: 100%;
 }
 </style>
